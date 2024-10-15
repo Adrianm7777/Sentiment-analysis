7f05a26e0be388d6487d39834258820d61ea25dd
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-15 17:08:51 +0200

    Fixed: incorrected import and typo

--- backend/sentiment/serializers.py
+++ backend/sentiment/serializers.py
@@ -1,4 +1,4 @@
 from rest_framework import serializers
 
 class SentimentSerializers(serializers.Serializer):
-    text = serializers.CharField(max_lengt=500)
+    text = serializers.CharField(max_length=500)
--- backend/sentiment/views.py
+++ backend/sentiment/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from serializers import SentimentSerializers
+from .serializers import SentimentSerializers
 
 def analyze_sentiment(text):
     return {'sentiment':'neutral'}
