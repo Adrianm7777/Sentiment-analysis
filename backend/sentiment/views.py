5e2919709af2f7df27daba6c29047dcea3393f7d
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-15 16:33:27 +0200

    Feature: Added logic for processing the text

--- backend/sentiment/views.py
+++ backend/sentiment/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from serializers import SentimentSerializers
 
-# Create your views here.
+def analyze_sentiment(text):
+    return {'sentiment':'neutral'}
+
+@api_view(['POST'])
+def analyze_text(request):
+    seralizer = SentimentSerializers(data=request.data)
+
+    if seralizer.is_valid():
+        text = seralizer.validated_data['text']
+        result = analyze_sentiment(text)
+
+        return Response(result)
+    return Response(seralizer.errors, status=400)
